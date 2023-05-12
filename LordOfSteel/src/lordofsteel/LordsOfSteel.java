042ee3ed6816e5a2ef602e3c99234cd426a73922
Alexyz17 <122872140+Alexyz17@users.noreply.github.com>
2023-05-12 17:14:50 +0200

    Commit inicial Commit hola Mejora del juego Copy del git (lo de clase)

---
+++ LordOfSteel/src/lordofsteel/LordsOfSteel.java
@@ -0,0 +1,287 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lordofsteel;
+
+/**
+ *
+ * @author alex
+ */
+public class LordsofSteel {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Nan n1 = new Nan("Tirion",8,6, 13,16,16,new Armes("Daga"));        
+        Huma h1 = new Huma("Luis",13,16,6,10,6,new Armes("Espasa"));
+        Mitja mi1 = new Mitja("Frodo",9,7,7,15,16,new Armes("Martell"));
+        Maia ma1 = new Maia("Nil",7,9, 12,15,17,new Armes("Daga"));
+
+        ArrayList<Personatge> personatges = new ArrayList<Personatge>();
+        personatges.add(n1);
+        personatges.add(h1);
+        personatges.add(mi1);
+        personatges.add(ma1);
+        int opcio;
+        do{
+        /* Menú principal */
+        System.out.println("");
+        System.out.println("** MENÚ PRINCIPAL **");
+        System.out.println("1.- Afegir personatge");
+        System.out.println("2.- Esborrar personatge");
+        System.out.println("3.- Editar personatge");
+        System.out.println("4.- Iniciar combat");
+        System.out.println("5.- Sortir");
+        System.out.println("**__________________**");
+        System.out.print("Tria opció [1-5]: ");
+        
+        String entrada = sc.nextLine();
+        
+        // Només hem d'arribar aquí si estem segurs
+        // que l'opció triada és un enter entre 1-5
+        opcio = Integer.parseInt(entrada);
+              
+        switch (opcio) {
+            case 1:
+                agregaPersonatge(personatges);
+                break;
+            case 2:
+                borrarPersonatge(personatges);
+                break;
+            case 3:
+                editaPersonatge(personatges);
+                break;
+            case 4:
+                iniciarCombat(personatges);
+                break;
+            case 5:
+                System.out.println("¡Ha triat sortit!");
+                break;
+            default:
+                System.out.println("Opcio invalida.");
+        }
+        }while(opcio !=5);    
+    }
+    
+    private static void agregaPersonatge(ArrayList<Personatge> personatges){
+        int categoria;
+        System.out.println("Introdueix la categoria del personatge: ");
+        System.out.println("1: Nan /n"
+                + "2: Humá /n"
+                + "3: Mitjà/n"
+                + "4: Maia");
+        categoria = sc.nextInt();
+        
+        int devocion;
+        System.out.println("Introdueix la devoció del personatge: ");
+        System.out.println("1: Ordre o 2: Caos");
+        devocion = sc.nextInt();
+        
+        
+        System.out.println("Introdueix el nom del personatge: ");
+        String nom=sc.nextLine();
+        sc.nextLine();
+        int fuerza, constitucion, velocidad, inteligencia, suerte, totalPunts;
+        do{
+            System.out.println("Reparteix 60 punts entre les estadístiques primàries:");
+            System.out.println("Força: ");
+            fuerza = sc.nextInt();
+            System.out.println("Constitució: ");
+            constitucion = sc.nextInt();
+            System.out.println("Velocitat: ");
+            velocidad = sc.nextInt();
+            System.out.println("Intel·ligència: ");
+            inteligencia = sc.nextInt();
+            System.out.println("Sort: ");
+            suerte = sc.nextInt();
+            
+            totalPunts = fuerza+constitucion+velocidad+inteligencia+suerte;
+            if (totalPunts!=60){
+                System.out.println("Assegura't de repartir un total de 60 punts entre les estadístiques primàries: força, destresa, constitució i agilitat.");                
+            }
+        }while(totalPunts!=60);
+            System.out.println("Introdueix el nom de l'arma: ");
+            String nomArma = sc.nextLine();
+        
+        switch (categoria) {
+            case 1:
+                {
+                    if(devocion==1){
+                        NanOrdre personatge = new NanOrdre(nom,fuerza,constitucion, velocidad,inteligencia ,suerte,new Armes(nomArma));
+                        personatges.add(personatge);
+                    }else{
+                        NanCaos personatge = new NanCaos(nom,fuerza,constitucion, velocidad,inteligencia ,suerte,new Armes(nomArma));
+                        personatges.add(personatge);
+                    }
+                    break;
+                }
+            case 2:
+                {
+                    Huma personatge = new Huma(nom,fuerza,constitucion, velocidad,inteligencia ,suerte,new Armes(nomArma));
+                    personatges.add(personatge);
+                    break;
+                }
+            case 3:
+                {
+                    Mitja personatge = new Mitja(nom,fuerza,constitucion, velocidad,inteligencia ,suerte,new Armes(nomArma));
+                    personatges.add(personatge);
+                    break;
+                }
+            case 4:
+                {
+                    Maia personatge = new Maia(nom,fuerza,constitucion, velocidad,inteligencia ,suerte,new Armes(nomArma));
+                    personatges.add(personatge);
+                    break;
+                }
+            default:
+                break;
+        }
+            System.out.println("S'ha afegit el personatge amb èxit.");
+        /*mirar de guaradr el personaje, caos o orden*/
+    }
+    
+    private static void borrarPersonatge(ArrayList<Personatge> personatges){
+        System.out.println("----- LLISTA DE PERSONATGES DISPONIBLES -----");
+        for (int i = 0; i < personatges.size(); ++i) {
+            System.out.println((i+1)+ ". "+ personatges.get(i).getNOM());            
+        }
+        System.out.println("-----------------------------------");
+        boolean valido = false;
+        do{
+            System.out.println("Si us plau, seleccioneu el número del personatge que voleu eliminar: ");
+            int seleccio = sc.nextInt() -1;
+
+            if(seleccio>=0 && seleccio<personatges.size()){
+                personatges.remove(seleccio);
+                System.out.println("S'ha eliminat el personatge amb èxit.");
+                valido=true;
+            } else {
+                System.out.println("Ho sento, el número de personatge no és vàlid. Si us plau, seleccioneu un número de personatge vàlid per continuar.");
+            }
+        } while(valido);
+        System.out.println("");
+    }
+    
+    private static void editaPersonatge(ArrayList<Personatge> personatges){
+        System.out.println("----- LLISTA DE PERSONATGES DISPONIBLES -----");
+        for (int i = 0; i < personatges.size(); ++i) {
+            System.out.println((i+1)+ ". "+ personatges.get(i).getNOM());            
+        }
+        System.out.println("-----------------------------------");
+        boolean valido = false;
+        System.out.println("Si us plau, seleccioneu el número del personatge que voleu eliminar: ");
+        int seleccio = sc.nextInt() -1;
+        if(seleccio>=0 && seleccio<personatges.size()){
+            Personatge personatge = personatges.get(seleccio);
+            System.out.println("Estadístiques actuals del personatge:");
+            System.out.println(personatge);
+            int newFuerza, newConstitucion, newVelocidad, newInteligencia, newSuerte;
+            do{
+                System.out.println("Introdueix el nou valor de Força:");
+                newFuerza=sc.nextInt();
+                System.out.println("Introdueix el nou valor de Constitució:");
+                newConstitucion=sc.nextInt();
+                System.out.println("Introdueix el nou valor de Velocitat:");
+                newVelocidad=sc.nextInt();
+                System.out.println("Introdueix el nou valor de Intel·ligència:");
+                newInteligencia=sc.nextInt();
+                System.out.println("Introdueix el nou valor de Sort:");
+                newSuerte=sc.nextInt();
+            int totalPunts = newFuerza+newConstitucion+newVelocidad+newInteligencia+newSuerte;
+            if(totalPunts <=60){    
+                valido = true;
+            }else{
+                System.out.println("Ho sento, però el total de punts excedeix els punts disponibles. Si us plau, torna-ho a intentar distribuint els punts de manera que la suma total no superi el límit disponible.");
+            }
+            }while(valido);
+            
+            personatge.setFOR(newFuerza);
+            personatge.setCON(newConstitucion);
+            personatge.setVEL(newVelocidad);
+            personatge.setINT(newInteligencia);
+            personatge.setSOR(newSuerte);
+            
+            System.out.println("Les estadístiques del personatge s'han actualitzat amb èxit.");
+            System.out.println("");
+        }
+    }
+    
+    private static void iniciarCombat(ArrayList<Personatge> personatges) {
+        
+        boolean[] seleccionats = new boolean[personatges.size()];
+        Personatge[] lluitadors = new Personatge[2];
+        for (int selec = 1; selec <= 2; selec++) {
+            for (int i = 0; i < personatges.size(); ++i) {
+                if (!seleccionats[i]) {
+                    String tipus = "";
+                    if (personatges.get(i) instanceof Nan)
+                        tipus = "Nan";
+                    else if (personatges.get(i) instanceof Huma)
+                        tipus = "Huma";
+                    else if (personatges.get(i) instanceof Mitja)
+                        tipus = "Mitja";
+                    else if (personatges.get(i) instanceof Maia)
+                        tipus = "Maia";
+
+                    System.out.printf("%d %s (%s)\n",(i+1),personatges.get(i).getNOM(),
+                                                     tipus);
+                }            
+            }
+            System.out.print("\nTria un personatge " + selec + ": ");
+            int opcio = sc.nextInt();
+            seleccionats[opcio-1] = true;
+            lluitadors[selec-1] = personatges.get(opcio-1);
+            System.out.println("Personatge triat: " + 
+                              personatges.get(opcio-1).getNOM());        
+        }
+        
+        
+        // Inici combat
+        Personatge atacant  = lluitadors[0];
+        Personatge defensor = lluitadors[1];
+        
+        Dau dau1 = new Dau();
+        Dau dau2 = new Dau();
+        Dau dau3 = new Dau();
+        
+        int valor = dau1.llencar() + dau2.llencar() + dau3.llencar();
+        //System.out.println("Valor daus: " + valor);
+        
+        if (valor <= atacant.getPA()) { // Atacant ataca
+            valor = dau1.llencar() + dau2.llencar() + dau3.llencar();
+            if (valor > defensor.getPE()) { // No aconsegueix esquivar
+                defensor.setPS(defensor.getPS() - atacant.getPD());
+            }
+        }
+        
+        // Final ronda
+        Personatge aux = atacant;
+        atacant  = defensor;
+        defensor = aux;
+        if (valor <= atacant.getPA()) { // Atacant ataca
+            valor = dau1.llencar() + dau2.llencar() + dau3.llencar();
+            if (valor > defensor.getPE()) { // No aconsegueix esquivar
+                defensor.setPS(defensor.getPS() - atacant.getPD());
+            }
+        }
+        
+        Personatge ganador, perdedor;
+        if(defensor.getPS() <=0){
+            ganador = atacant;
+            perdedor = defensor;
+        }else{
+            ganador = defensor;
+            perdedor = atacant;
+        }
+        System.out.println("¡La batalla ha acabat!");
+        System.out.println("El guanyador es: "+ganador.getNOM());
+        System.out.println("------------------------");
+        
+        ganador.restaurarPS();
+        perdedor.restaurarPS();
+        /*revisar*/
+        /*mirar que los datos interoducidos sean correctos*/
+        
+        
+    }
+
+}
--- LordOfSteel/src/lordofsteel/Personaje.java
+++ LordOfSteel/src/lordofsteel/Personaje.java
@@ -1,200 +1,160 @@
 package lordofsteel;
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
  * @author alex
  */
-public class Personaje {
-    private String nombre;
-    private String categoria;
-    private String arma;
-    private int experiencia;
-    private int nivel;
-    private int FOR;
-    private int CON;
-    private int VEL;
-    private int INT;
-    private int SOR;
-    private int WPOW;
-    private int WVEL;
-    private int PS;
-    private int PD;
-    private int PA;
-    private int PE;
+public class Personatge {
+    
+    protected int FOR; 
+    protected int CON;
+    protected int VEL; 
+    protected int INT; 
+    protected int SOR;
 
-    // Constructor
-    public Personaje(String nombre, String categoria, String arma, int experiencia, int nivel, int FOR, int CON, int VEL, int INT, int SOR) {
-        this.nombre = nombre;
-        this.categoria = categoria;
-        this.arma = arma;
-        this.experiencia = experiencia;
-        this.nivel = nivel;
+    protected Armes arma;
+    
+    protected int PS;
+    protected int PD; 
+    protected int PA; 
+    protected int PE;
+    
+    protected int WPOW;
+    protected int WVEL;
+    
+    protected String NOM;
+
+    
+    public Personatge (String NOM, int FOR, int CON, int VEL, int INT, int SOR, Armes arma){
+        this.NOM = NOM;
         this.FOR = FOR;
         this.CON = CON;
         this.VEL = VEL;
         this.INT = INT;
         this.SOR = SOR;
-    }
-
-    // Getters y Setters
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-
-    public String getArma() {
-        return arma;
-    }
-
-    public void setArma(String arma) {
         this.arma = arma;
+        
+        calculaEstadistiquesSecundaries();
     }
-
-    public int getExperiencia() {
-        return experiencia;
+    
+    protected void calculaEstadistiquesSecundaries(){
+        WPOW = arma.getWPOW();
+        WVEL = arma.getWVEL();
+        PS = CON + FOR;
+        PD = (FOR + WPOW) / 4;
+        PA = INT + SOR + WVEL;
+        PE = VEL + SOR + INT;   
     }
-
-    public void setExperiencia(int experiencia) {
-        this.experiencia = experiencia;
+    
+        public void restaurarPS(){
+            this.PS = (int)(this.PS*1.10);
     }
-
-    public int getNivel() {
-        return nivel;
-    }
-
-    public void setNivel(int nivel) {
-        this.nivel = nivel;
-    }
-
-    public int getFOR() {
-        return FOR;
-    }
-
+    
+    public void metodo(Dau... d){
+        d[0].llencar();
+        d[1].llencar();
+        d[2].llencar();
+    }    
+        
     public void setFOR(int FOR) {
         this.FOR = FOR;
-    }
-
-    public int getCON() {
-        return CON;
     }
 
     public void setCON(int CON) {
         this.CON = CON;
     }
 
-    public int getVEL() {
-        return VEL;
-    }
-
     public void setVEL(int VEL) {
         this.VEL = VEL;
-    }
-
-    public int getINT() {
-        return INT;
     }
 
     public void setINT(int INT) {
         this.INT = INT;
     }
 
-    public int getSOR() {
-        return SOR;
-    }
-
     public void setSOR(int SOR) {
         this.SOR = SOR;
     }
 
-    // Calcula la estadística secundaria PS (puntos de salud) a partir de CON y FOR
-    public int calcularPS() {
-        return CON + FOR;
+    public void setArma(Armes arma) {
+        this.arma = arma;
     }
 
-    // Calcula la estadística secundaria PD (puntos de daño) a partir de FOR, WPOW y arma
-    public int calcularPD(int WPOW) {
-        return (FOR + WPOW) / 4;
+    public void setPS(int PS) {
+        this.PS = PS;
     }
 
-    // Calcula la estadística secundaria PA (probabilidad de ataque) a partir de INT, SOR, WVEL y arma
-    public int calcularPA(int WVEL) {
-        return INT + SOR + WVEL;
+    public void setPD(int PD) {
+        this.PD = PD;
     }
 
-    // Calcula la estadística secundaria PE (probabilidad de esquivar) a partir de VEL, SOR, INT y FOR
-    public int calcularPE() {
-        return VEL + SOR + INT;
-    }
-    
-    // Actualiza el personaje al subir de nivel
-    public void subirNivel() {
-    // Incrementar el nivel del personaje
-        this.nivel++;
-    // Actualizar estadisticas primarias y secundarias
-        this.FOR++;
-        this.CON++;
-        this.VEL++;
-        this.INT++;
-        this.SOR++;
-        this.experiencia = 0;
-            actualizarEstadisticasSecundarias();
+    public void setPA(int PA) {
+        this.PA = PA;
     }
 
-    public int getPexSiguienteNivel(){
-        switch(this.nivel){
-            case 1:
-                return 100;
-            case 2:
-                return 200;
-            case 3:
-                return 500;
-            case 4:
-                return 1000;
-            case 5:
-                return 2000;
-            default:
-                return Integer.MAX_VALUE;
-        }
+    public void setPE(int PE) {
+        this.PE = PE;
     }
-    public void actualizarEstadisticasSecundarias() {
-        // Actualizar estadisticas secundarias
-            this.PS = this.CON + this.FOR;
-            this.PD = (this.FOR + this.WPOW) / 4;
-            this.PA = this.INT + this.SOR + this.WVEL;
-            this.PE = this.VEL + this.SOR + this.INT;
 
-        // Aplicar la bonificacion de la categoria del personaje
-            switch(this.categoria) {
-                case "Nan":
-                    this.PD = (this.FOR + this.WPOW + this.CON) / 4;
-                break;
-                case "Humà":
-                    this.PS = this.CON + this.FOR + this.INT;
-                break;
-                case "Mitjà":
-                     this.PE = this.VEL + this.SOR + this.INT + this.FOR + 2;
-                  break;
-                case "Maia":
-                  this.PA = this.INT + this.SOR + this.WVEL + this.VEL + 2;
-                  break;
-                default:
-                  break;
-            }
+    public int getFOR() {
+        return FOR;
+    }
+
+    public int getCON() {
+        return CON;
+    }
+
+    public int getVEL() {
+        return VEL;
+    }
+
+    public int getINT() {
+        return INT;
+    }
+
+    public int getSOR() {
+        return SOR;
+    }
+
+    public Armes getArma() {
+        return arma;
+    }
+
+    public int getPS() {
+        return PS;
+    }
+
+    public int getPD() {
+        return PD;
+    }
+
+    public int getPA() {
+        return PA;
+    }
+
+    public int getPE() {
+        return PE;
+    }
+        public void setWPOW(int WPOW) {
+        this.WPOW = WPOW;
+    }
+
+    public void setWVEL(int WVEL) {
+        this.WVEL = WVEL;
+    }
+
+    public void setNOM(String NOM) {
+        this.NOM = NOM;
+    }
+
+    public int getWPOW() {
+        return WPOW;
+    }
+
+    public int getWVEL() {
+        return WVEL;
+    }
+
+    public String getNOM() {
+        return NOM;
     }
 }
